570e0bc2944d5a43630909533bd577c2938e00d2
Benjamin Bergstrom <Benko11@users.noreply.github.com>
2017-08-16 16:01:19 +0200

    WOW

---
+++ functions/RandomGenerator.inc.php
@@ -0,0 +1,13 @@
+<?php
+	interface RandomGeneratorInterface {
+		public function __construct();
+
+		// public function generateOutput($characters);
+
+		public function string($length);     // everything except punctuation
+		public function lowString($length);  // lowercase letters
+		public function upString($length);   // uppercase letters
+		public function numbers($length);    // numbers
+		public function anyString($length);  // everything
+		public function myString($length, $rules);   // user-defined parameters
+	}
--- functions/RandomGenerator.php
+++ functions/RandomGenerator.php
@@ -1,12 +1,10 @@
 <?php
-	class RandomGenerator {
+	class RandomGenerator implements RandomGeneratorInterface  {
 		protected $characters = array(); // the list of characters that the string will be generated from
 		public $group = array();		 // the list of character groups
 
 		// use 'default' or 0 to access default restrictions or define your own
 		public $restrictions;
-
-		private $aliases;
 
 		/*
 			@return void
@@ -55,11 +53,110 @@
 		/*
 		@return array characters
 		*/
-		public function retrieveCharacters() {
-			
+		private function retrieve_characters($restrictions) {
+			$characters = array();
 
-			return $this->characters;
+			if (is_array($restrictions)) {
+				foreach ($restrictions as $r) {
+					if (is_numeric($r)) { // treated as group_id
+						foreach (str_split($this->group[$r]['list']) as $letter) {
+							$characters[] = $letter;
+						}
+					} else { // treated as additional characters to add
+						
+					}
+				}
+			}
+
+			return $characters;
+		}
+
+		/*
+		@return string output
+		*/
+		public function output($rules, $length) {
+			$characters = self::retrieve_characters($rules);
+
+			$output = '';
+
+			for ($i = 1;$i <= $length;$i++) {
+				$output .= $characters[rand(0, count($characters) - 1)];
+			}
+
+			return $output;
+		}
+
+		/*
+		@return string output
+		*/
+		public function string($length) {
+			$rules = [0, 1];
+			$output = self::output($rules, $length);
+
+			return $output;
+		}
+
+		public function mixedString($length) {
+			$rules = [0,1,2];
+			$output = self::output($rules, $length);
+
+			return $output;
+		}
+
+		/*
+		@return string output
+		*/
+		public function lowString($length) {
+			$rules = [0];
+			$output = self::output($rules, $length);
+
+			return $output;
+		}
+
+		/*
+		@return string output
+		*/
+		public function upString($length) {
+			$rules = [1];
+			$output = self::output($rules, $length);
+
+			return $output;
+		}
+
+		/*
+		@return string output
+		*/
+		public function numbers($length) {
+			$rules = [2];
+			$output = self::output($rules, $length);
+
+			return $output;
+		}
+
+		/*
+		@return string output
+		*/
+		public function punctuation($length) {
+			$rules = [3];
+			$output = self::output($rules, $length);
+
+			return $output;
+		}
+
+		/*
+		@return string output
+		*/
+		public function anyString($length) {
+			$rules = [0,1,2,3];
+			$output = self::output($rules, $length);
+
+			return $output;
+		}
+
+		/*
+		@return string output
+		*/
+		public function myString($length, $rules) {
+			
 		}
 	}
-
-	print_r((new RandomGenerator)->retrieveCharacters());
