6ebe5a6c67f85a888c6fee8ecd367319787fcf63
Benjamin Bergstrom <Benko11@users.noreply.github.com>
2017-08-16 15:16:08 +0200

    Go

--- functions/RandomGenerator.php
+++ functions/RandomGenerator.php
@@ -1,23 +1,64 @@
 <?php
 	class RandomGenerator {
-		protected $characters;
-		private $lowercaseLetters; // 0
-		private $uppercaseLetters; // 1
-		private $numbers;		   // 2
-		private $punctuation;	   // 3
+		protected $characters = array(); // the list of characters that the string will be generated from
+		public $group = array();		 // the list of character groups
+
+		// use 'default' or 0 to access default restrictions or define your own
+		public $restrictions;
+
+		private $aliases;
 
 		/*
 			@return void
 		*/
 		public function __construct() {
-			$this->numbers = str_split('0123456789');
-			$this->lowercaseLetters = str_split('abcdefghijklmnopqrstuvwxyz'); 
-			$this->uppercaseLetters = str_split('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-			$this->punctuation = str_split('!@#$%^&*()\'');
+			$this->group = [
+				0 => [
+					'name' 	  => 'lowercaseletters',
+					'list'    => 'abcdefghijklmnopqrstuvwxyz',
+					'aliases' => [
+						'lowletters',
+						'lowercase',
+						'low'
+					]
+				],
+
+				1 => [
+					'name' 	  => 'uppercaseletters',
+					'list' 	  => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+					'aliases' => [
+						'upletters',
+						'uppercase',
+						'up'
+					]
+				],
+
+				2 => [
+					'name' => 'numbers',
+					'list' => '0123456789',
+					'aliases' => [
+						'numerals',
+						'num'
+					]
+				],
+
+				3 => [
+					'name' => 'punctuation',
+					'list' => "!@#$%^&*()'",
+					'aliases' => [
+						'other'
+					]
+				]
+			];
 		}
 
+		/*
+		@return array characters
+		*/
 		public function retrieveCharacters() {
-			return $this->numbers;
+			
+
+			return $this->characters;
 		}
 	}
 
