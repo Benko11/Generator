a489851bde8dbbcf9ccd6f90c5808c570ef45e3e
Benjamin Bergstrom <Benko11@users.noreply.github.com>
2017-08-16 14:28:32 +0200

    In development

---
+++ functions/Generator.php
@@ -0,0 +1,24 @@
+<?php
+	class Generator {
+		protected $characters;
+		private $lowercaseLetters; // 0
+		private $uppercaseLetters; // 1
+		private $numbers;		   // 2
+		private $punctuation;	   // 3
+
+		/*
+			@return void
+		*/
+		public function __construct() {
+			$this->numbers = str_split('0123456789');
+			$this->lowercaseLetters = str_split('abcdefghijklmnopqrstuvwxyz'); 
+			$this->uppercaseLetters = str_split('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+			$this->punctuation = str_split('!@#$%^&*()\'');
+		}
+
+		public function retrieveCharacters() {
+			return $this->numbers;
+		}
+	}
+
+	print_r((new Generator)->retrieveCharacters());
